29d2647ed4bbab0ee0458751eaf10aff23f63f9a
Anthony Nordquist <salinelinux@gmail.com>
2012-03-31 14:01:19 -0400

    New packages

---
+++ midori-0.4.4/midori-0.4.4/midori/sokoke.h
@@ -0,0 +1,205 @@
+/*
+ Copyright (C) 2007-2008 Christian Dywan <christian@twotoasts.de>
+ Copyright (C) 2009 Dale Whittaker <dayul@users.sf.net>
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ See the file COPYING for the full license text.
+*/
+
+#ifndef __SOKOKE_H__
+#define __SOKOKE_H__ 1
+
+#include <JavaScriptCore/JavaScript.h>
+#include <midori/midori-websettings.h>
+#include <katze/gtk3-compat.h>
+
+gchar*
+sokoke_js_script_eval                   (JSContextRef    js_context,
+                                         const gchar*    script,
+                                         gchar**         exception);
+
+void
+sokoke_message_dialog                   (GtkMessageType  message_type,
+                                         const gchar*    short_message,
+                                         const gchar*    detailed_message,
+                                         gboolean        modal);
+
+gboolean
+sokoke_show_uri_with_mime_type          (GdkScreen*      screen,
+                                         const gchar*    uri,
+                                         const gchar*    mime_type,
+                                         guint32         timestamp,
+                                         GError**        error);
+
+gboolean
+sokoke_show_uri                         (GdkScreen*      screen,
+                                         const gchar*    uri,
+                                         guint32         timestamp,
+                                         GError**        error);
+
+gboolean
+sokoke_spawn_program                    (const gchar* command,
+                                         const gchar* argument);
+
+void
+sokoke_spawn_app                        (const gchar*    uri,
+                                         gboolean        inherit_config);
+
+gboolean
+sokoke_external_uri                     (const gchar*    uri);
+
+gchar*
+sokoke_magic_uri                        (const gchar*    uri);
+
+void
+sokoke_widget_set_visible               (GtkWidget*      widget,
+                                         gboolean        visible);
+
+GtkWidget*
+sokoke_xfce_header_new                  (const gchar*    icon,
+                                         const gchar*    title);
+
+void
+sokoke_entry_set_default_text           (GtkEntry*       entry,
+                                         const gchar*    default_text);
+
+gchar*
+sokoke_key_file_get_string_default      (GKeyFile*       key_file,
+                                         const gchar*    group,
+                                         const gchar*    key,
+                                         const gchar*    default_value,
+                                         GError**        error);
+
+gint
+sokoke_key_file_get_integer_default     (GKeyFile*       key_file,
+                                         const gchar*    group,
+                                         const gchar*    key,
+                                         const gint      default_value,
+                                         GError**        error);
+
+gdouble
+sokoke_key_file_get_double_default      (GKeyFile*       key_file,
+                                         const gchar*    group,
+                                         const gchar*    key,
+                                         gdouble         default_value,
+                                         GError**        error);
+
+gboolean
+sokoke_key_file_get_boolean_default     (GKeyFile*       key_file,
+                                         const gchar*    group,
+                                         const gchar*    key,
+                                         gboolean        default_value,
+                                         GError**        error);
+
+gchar**
+sokoke_key_file_get_string_list_default (GKeyFile*       key_file,
+                                         const gchar*    group,
+                                         const gchar*    key,
+                                         gsize*          length,
+                                         gchar**         default_value,
+                                         gsize*          default_length,
+                                         GError*         error);
+
+gboolean
+sokoke_key_file_save_to_file            (GKeyFile*       key_file,
+                                         const gchar*    filename,
+                                         GError**        error);
+
+void
+sokoke_widget_get_text_size             (GtkWidget*      widget,
+                                         const gchar*    text,
+                                         gint*           width,
+                                         gint*           height);
+
+GtkWidget*
+sokoke_action_create_popup_menu_item    (GtkAction*      action);
+
+gint64
+sokoke_time_t_to_julian                 (const time_t*   timestamp);
+
+const gchar*
+sokoke_set_config_dir                   (const gchar*    new_config_dir);
+
+gboolean
+sokoke_is_app_or_private                (void);
+
+gboolean
+sokoke_remove_path                      (const gchar*    path,
+                                         gboolean        ignore_errors);
+
+gchar*
+sokoke_find_config_filename             (const gchar*    folder,
+                                         const gchar*    filename);
+
+gchar*
+sokoke_find_lib_path                    (const gchar*    folder);
+
+gchar*
+sokoke_find_data_filename               (const gchar*    filename,
+                                         gboolean        res);
+
+gchar**
+sokoke_get_argv                         (gchar**         argument_vector);
+
+gchar*
+sokoke_replace_variables                (const gchar* template,
+                                         const gchar* variable_first, ...);
+
+gboolean
+sokoke_window_activate_key              (GtkWindow*      window,
+                                         GdkEventKey*    event);
+guint
+sokoke_gtk_action_count_modifiers       (GtkAction* action);
+
+GtkWidget*
+sokoke_file_chooser_dialog_new          (const gchar*         title,
+                                         GtkWindow*           window,
+                                         GtkFileChooserAction action);
+
+gboolean
+sokoke_prefetch_uri                     (MidoriWebSettings*  settings,
+                                         const char*         uri,
+                                         SoupAddressCallback callback,
+                                         gpointer            user_data);
+
+gboolean
+sokoke_resolve_hostname                 (const gchar*        hostname);
+
+gchar *
+sokoke_accept_languages                 (const gchar* const * lang_names);
+
+gboolean
+sokoke_recursive_fork_protection        (const gchar*         uri,
+                                         gboolean             set_uri);
+
+typedef struct
+{
+    gchar* name;
+    gchar* label;
+    GCallback clear;
+} SokokePrivacyItem;
+
+GList*
+sokoke_register_privacy_item (const gchar* name,
+                              const gchar* label,
+                              GCallback    clear);
+
+void
+sokoke_widget_copy_clipboard (GtkWidget*   widget,
+                              const gchar* text);
+
+gchar*
+sokoke_build_thumbnail_path (const gchar* name);
+
+gchar*
+midori_download_prepare_tooltip_text (WebKitDownload* download);
+
+void
+sokoke_entry_set_clear_button_visible (GtkEntry*           entry,
+                                       gboolean            visible);
+
+#endif /* !__SOKOKE_H__ */
--- midori-0.4.4/midori_0.4.4.dsc
+++ midori-0.4.4/midori_0.4.4.dsc
@@ -1,4 +1,4 @@
-Format: 1.0
+Format: 3.0 (quilt)
 Source: midori
 Binary: midori
 Architecture: any
@@ -7,8 +7,11 @@ Maintainer: Anthony Nordquist <salinelinux@gmail.com>
 Standards-Version: 3.9.1
 Build-Depends: valac, intltool, libgtk2.0-dev, libwebkit-dev, libsqlite3-dev, debhelper (>= 8)
 Checksums-Sha1: 
- 1c64809702da4b53c81203a733aeb93338f01036 1780810 midori_0.4.4.tar.gz
+ 5777bf7ee9591451282b2198bd8d6df6af22eca3 1429675 midori_0.4.4.orig.tar.gz
+ aa415a1205794bbbe8692453d1cd0708445763ea 332296 midori_0.4.4.debian.tar.gz
 Checksums-Sha256: 
- bbaf92edb6a1cd6e3a1f65297c4c20fb0d7fdfc528b8181e73b74090b8b48024 1780810 midori_0.4.4.tar.gz
+ f154007f2894da7acbc10748124c06419603f57e5239d39ed368fa38d451fd20 1429675 midori_0.4.4.orig.tar.gz
+ 7857cc5dc75caf85ce6c93165a9f454b1ff83065aeafb3b0fb5a11a09055e4a0 332296 midori_0.4.4.debian.tar.gz
 Files: 
- 8dafa0f7f4b7c2cfc671427ddaf31033 1780810 midori_0.4.4.tar.gz
+ ea7e39cf69ca366e83680a4afe14b89b 1429675 midori_0.4.4.orig.tar.gz
+ 42aa582e8db70b9ab597d4f15cd05159 332296 midori_0.4.4.debian.tar.gz
